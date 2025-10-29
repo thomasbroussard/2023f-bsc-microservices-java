a98c3b5514351ed618bd25320cfece826fa4d191
Présentation <tbr@tbr.com>
2025-10-29 09:45:41 +0100

    conversion of training-exam to maven

--- calculator-example/src/main/java/fr/epita/calculator/service/Calculator.java
+++ calculator-example/src/main/java/fr/epita/calculator/service/Calculator.java
@@ -1,8 +1,15 @@
 package fr.epita.calculator.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculator {
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
+
+
     public double divide(int operand1, int operand2) {
         if (operand2 == 0) {
+            logger.error("Divide by zero");
             throw new ArithmeticException("Divide by zero");
         }
         return ((double)operand1) / operand2;
@@ -10,12 +17,14 @@ public class Calculator {
 
     public int add(String s) {
         if ("".equals(s) || s == null) {
+            logger.warn("input was empty or null");
             return 0;
         }
         String[] numbers = s.split(",");
         if (numbers.length == 1) {
             return Integer.parseInt(s);
         } else {
+            logger.error("this is not implemented");
             throw new IllegalStateException("not yet implemented");
         }
     }
