347eaa36e7615c9a64e7bb486956667947f16cbb
Présentation <tbr@tbr.com>
2025-11-03 14:24:02 +0100

    introducing hibernate

--- quiz-core/src/test/java/fr/epita/quiz/tests/TestConfiguration.java
+++ quiz-core/src/test/java/fr/epita/quiz/tests/TestConfiguration.java
@@ -1,15 +1,19 @@
 package fr.epita.quiz.tests;
 
 import fr.epita.quiz.services.DataAccessService;
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.sql.DriverManager;
+import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
 public class TestConfiguration {
 
 
@@ -22,19 +26,36 @@ public class TestConfiguration {
     public DataSource connectionProvider(){
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
         driverManagerDataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
-        //password, user, etc...
+        driverManagerDataSource.setUsername("test");
+        driverManagerDataSource.setPassword("test");
+        driverManagerDataSource.setDriverClassName("org.h2.Driver");
         return driverManagerDataSource;
     }
 
     @Bean
-    public LocalSessionFactoryBean sessionFactoryBean(DataSource dataSource){
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource){
         LocalSessionFactoryBean sfBean = new LocalSessionFactoryBean();
         sfBean.setDataSource(dataSource);
         sfBean.setPackagesToScan("fr.epita.quiz.datamodel");
 
-        return sfBean ;
+        // Configure Hibernate properties
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        hibernateProperties.setProperty("hibernate.format_sql", "true");
+
+        sfBean.setHibernateProperties(hibernateProperties);
+
+        return sfBean;
     }
 
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory){
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+        return transactionManager;
+    }
 
     @Bean
     public DataAccessService dataAccessService(DataSource dataSource){
--- quiz-core/src/test/java/fr/epita/quiz/tests/TestHibernate.java
+++ quiz-core/src/test/java/fr/epita/quiz/tests/TestHibernate.java
@@ -1,16 +1,22 @@
 package fr.epita.quiz.tests;
 
 import fr.epita.quiz.datamodel.Question;
-import jakarta.persistence.Entity;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestConfiguration.class)
@@ -22,19 +28,30 @@ public class TestHibernate {
     @Autowired
     DataSource dataSource;
 
-    public void test() {
+    @Test
+    public void test() throws Exception {
         //given
         Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
         Question question = new Question();
         question.setTitle("Question 1");
 
         //when
         session.persist(question);
+        session.merge(question);
+        session.remove(question);
+        session.find(Question.class, question.getId());
+
+
+        transaction.commit();
+        session.close();
 
         //then
         PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement("SELECT * FROM QUESTIONS");
+        ResultSet resultSet = preparedStatement.executeQuery();
 
-
+        assertTrue(resultSet.next(), "At least one question should be found");
+        assertNotNull(resultSet.getString("TITLE"), "Question title should not be null");
     }
 
 
